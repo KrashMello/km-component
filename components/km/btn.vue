c51348e734660f8ac0aaadb7edf49593bea4cba4
krashmello <joelenrikesoteldo@gmail.com>
2024-01-03 06:48:41 -0400

    fix: "outlined style in btn"

--- components/km/btn.vue
+++ components/km/btn.vue
@@ -30,7 +30,8 @@ ${
     : ''
 }
 ${xSmall ? 'h-5 text-xs' : small ? 'h-7 text-sm ' : 'h-10'} 
-font-semibold hover:bg-opacity-90 active:bg-opacity-75 transition hover:transition-colors duration-400 
+font-semibold hover:bg-opacity-90 active:bg-opacity-75
+transition hover:transition-colors hover:scale-110 duration-200 ease-in
 ${!isColor(color) ? color : colors[color]}
 ${width} 
 ${roundedSize[rounded]}
@@ -64,43 +65,44 @@ type types = "link" | "button" | "anchor";
 type targets = "_blank" | "_self" | "_parent" | "_top" | "framename";
 type roundedType = "full" | "top" | "bottom" | "left" | "right" | "none";
 type colorType = "danger" | "warning" | "info" | "success" | "blue" | "gray";
+type borderType = "top" | "up" | "left" | "right" | "none";
 export default {
   setup(props) {
     const { outlined } = props;
     const colors = {
       red: `border-red-500 ${
         outlined
-          ? "text-red-400 active:bg-red-500/75 active:text-white hover:text-white hover:bg-red-500"
+          ? "text-red-400 active:bg-red-500/75 active:text-white hover:text-white hover:bg-red-500/30"
           : "bg-red-400"
       }`,
       danger: ` border-red-500 ${
         outlined
-          ? "text-red-400 active:bg-red-500/75 active:text-white hover:text-white hover:bg-red-500"
+          ? "text-red-400 active:bg-red-500/75 active:text-white hover:text-white hover:bg-red-500/30"
           : "bg-red-400"
       }`,
       success: ` border-green-500 ${
         outlined
-          ? "text-green-400 active:bg-green-500/75 active:text-white hover:text-white hover:bg-green-500"
+          ? "text-green-400 active:bg-green-500/75 active:text-white hover:text-white hover:bg-green-500/30"
           : "bg-green-400"
       }`,
       info: `border-blue-500 ${
         outlined
-          ? "text-blue-400 active:bg-blue-500/75 active:text-white hover:text-white hover:bg-blue-500"
+          ? "text-blue-400 active:bg-blue-500/75 active:text-white hover:text-white hover:bg-blue-500/30"
           : "bg-blue-400"
       }`,
       blue: `border-blue-400 ${
         outlined
-          ? "text-blue-600 active:bg-blue-400/75 active:text-white hover:text-white hover:bg-blue-400"
+          ? "text-blue-600 active:bg-blue-400/75 active:text-white hover:text-white hover:bg-blue-400/30"
           : "bg-blue-600"
       }`,
       warning: `border-yellow-500 ${
         outlined
-          ? "text-yellow-400 active:bg-yellow-500/75 active:text-white hover:text-white hover:bg-yellow-400"
+          ? "text-yellow-400 active:bg-yellow-500/75 active:text-white hover:text-white hover:bg-yellow-400/30"
           : "bg-yellow-400"
       }`,
       gray: `border-gray-500 ${
         outlined
-          ? "text-gray-400 active:bg-gray-500/75 active:text-white hover:text-white hover:bg-gray-400"
+          ? "text-gray-400 active:bg-gray-500/75 active:text-white hover:text-white hover:bg-gray-400/30"
           : "bg-gray-400 "
       }`,
       transparent: `bg-transparent text-white hover:bg-white/40 ${
@@ -151,6 +153,10 @@ export default {
     outlined: {
       type: Boolean,
       default: false,
+    },
+    border: {
+      type: String as PropType<borderType>,
+      default: "none",
     },
   },
   data() {
