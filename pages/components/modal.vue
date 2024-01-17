69f4b89baacb715ce45d29b402aa444f51bc63a0
Enyelberth Rodriguez <enyelberthrc22.z@gmail.com>
2024-01-17 18:56:08 -0400

    asda
    asdd

---
+++ pages/components/modal.vue
@@ -0,0 +1,21 @@
+<template>
+    <layouts-sections-components>
+      <div class="flex flex-col gap-4">
+        <h2 class="text-center text-6xl font-bold">Alertas</h2>
+        <km-alert color="danger"> esta es una etiqueta de peligro :) </km-alert>
+      </div>
+    </layouts-sections-components>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from "vue";
+  
+  export default defineComponent({
+    setup() {
+      return {};
+    },
+  });
+  </script>
+  
+  <style scoped></style>
+  
