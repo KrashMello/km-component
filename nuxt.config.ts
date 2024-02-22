27b0598d24c903a7a1fd070e07f00ee63bc32690
krashmello <joelenrikesoteldo@gmail.com>
2024-02-22 06:57:09 -0400

    docs: add and adapter new markdown with nuxt content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@nuxt/content"],
   pinia: {
     autoImports: ["defineStore", "definePiniaStore"],
   },
   imports: {
     dirs: ["store"],
   },
-  ssr: false,
+  content: {
+    highlight: {
+      theme: {
+        default: "one-dark-pro",
+        dark: "github-dark",
+      },
+    },
+  },
   tailwindcss: {
     cssPath: "~/assets/tailwind.css",
     configPath: "@/tailwind.config.ts",
