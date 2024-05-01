d6cf2d75cf4990ccb4b4b05a4b8d4569fd1c1ae5
krashmello <joelenrikesoteldo@gmail.com>
2024-05-01 17:45:19 -0400

    feat: add md descriptions

--- pages/components/button.vue
+++ pages/components/button.vue
@@ -1,54 +1,182 @@
 <template>
   <layouts-sections-components>
     <div class="flex flex-col gap-4">
-      <h2 class="text-center text-6xl font-bold">Botones</h2>
-      <h3 class="text-3xl font-semibold">normales</h3>
-      <div class="flex flex-wrap gap-3">
-        <km-btn>color info</km-btn>
-        <km-btn color="danger">color danger</km-btn>
-        <km-btn color="success">color success</km-btn>
-        <km-btn color="warning">color warning</km-btn>
-        <km-btn color="gray">color gray</km-btn>
-        <km-btn color="purple">color purple</km-btn>
-        <km-btn color="transparent">transparent</km-btn>
-      </div>
-      <h3 class="text-3xl font-semibold">rounded</h3>
-      <div class="flex flex-wrap gap-3">
-        <km-btn rounded="full">color danger</km-btn>
-        <km-btn rounded="bottom">color success</km-btn>
-        <km-btn rounded="top">color warning</km-btn>
-        <km-btn rounded="left">color gray</km-btn>
-        <km-btn rounded="right">transparent</km-btn>
-      </div>
-      <h3 class="text-3xl font-semibold">outlined</h3>
-      <div class="flex flex-wrap gap-3">
-        <km-btn outlined>info</km-btn>
-        <km-btn outlined color="danger">danger</km-btn>
-        <km-btn outlined color="success">success</km-btn>
-        <km-btn outlined color="warning">warning</km-btn>
-        <km-btn outlined color="transparent">transparent</km-btn>
-      </div>
-      <h3 class="text-3xl font-semibold">tamaños</h3>
-      <div class="flex flex-wrap gap-3">
-        <km-btn x-small>extra small</km-btn>
-        <km-btn small>small</km-btn>
-        <km-btn>normal</km-btn>
-      </div>
-      <h3 class="text-3xl font-semibold">bordes</h3>
-      <div class="flex flex-wrap gap-3">
-        <km-btn border="top" color="warning">border top</km-btn>
-        <km-btn border="left" color="danger">border left</km-btn>
-        <km-btn border="right" color="success">border right</km-btn>
-        <km-btn border="bottom" color="info">border bottom</km-btn>
-      </div>
+      <h2 class="text-center text-6xl font-bold">Buttons</h2>
+      <km-tabs-group :tabs="componentTabsLists" id="component-buttons">
+        <km-tabs-content id="components">
+          <ContentRendererMarkdown class="text-xs" :value="MDComponents" />
+        </km-tabs-content>
+      </km-tabs-group>
+      <h3 class="text-3xl font-semibold">Flats</h3>
+      <km-tabs-group :tabs="flatTabsLists" id="flats-buttons">
+        <km-tabs-content id="flat-example">
+          <div class="flex flex-wrap gap-3">
+            <km-btn>color info</km-btn>
+            <km-btn color="danger">color danger</km-btn>
+            <km-btn color="success">color success</km-btn>
+            <km-btn color="warning">color warning</km-btn>
+            <km-btn color="gray">color gray</km-btn>
+            <km-btn color="purple">color purple</km-btn>
+            <km-btn color="transparent">transparent</km-btn>
+          </div>
+        </km-tabs-content>
+        <km-tabs-content id="flat-vue">
+          <ContentRendererMarkdown class="text-xs" :value="MDFlatComponents" />
+        </km-tabs-content>
+      </km-tabs-group>
+
+      <h3 class="text-3xl font-semibold">Rounded</h3>
+      <km-tabs-group :tabs="roundedTabsLists" id="rounded-buttons">
+        <km-tabs-content id="rounded-example">
+          <div class="flex flex-wrap gap-3">
+            <km-btn rounded="full">color danger</km-btn>
+            <km-btn rounded="bottom">color success</km-btn>
+            <km-btn rounded="top">color warning</km-btn>
+            <km-btn rounded="left">color gray</km-btn>
+            <km-btn rounded="right">transparent</km-btn>
+          </div>
+        </km-tabs-content>
+        <km-tabs-content id="rounded-vue">
+          <ContentRendererMarkdown
+            class="text-xs"
+            :value="MDRoundedComponents"
+          />
+        </km-tabs-content>
+      </km-tabs-group>
+      <h3 class="text-3xl font-semibold">Outlined</h3>
+      <km-tabs-group :tabs="outlinedTabsLists" id="outlined-buttons">
+        <km-tabs-content id="outlined-example">
+          <div class="flex flex-wrap gap-3">
+            <km-btn outlined>info</km-btn>
+            <km-btn outlined color="danger">danger</km-btn>
+            <km-btn outlined color="success">success</km-btn>
+            <km-btn outlined color="warning">warning</km-btn>
+            <km-btn outlined color="transparent">transparent</km-btn>
+          </div>
+        </km-tabs-content>
+        <km-tabs-content id="outlined-vue">
+          <ContentRendererMarkdown
+            class="text-xs"
+            :value="MDOutlinedComponents"
+          />
+        </km-tabs-content>
+      </km-tabs-group>
+
+      <h3 class="text-3xl font-semibold">Sizes</h3>
+
+      <km-tabs-group :tabs="sizeTabsLists" id="size-buttons">
+        <km-tabs-content id="size-example">
+          <div class="flex flex-wrap gap-3">
+            <km-btn x-small>extra small</km-btn>
+            <km-btn small>small</km-btn>
+            <km-btn>normal</km-btn>
+          </div>
+        </km-tabs-content>
+        <km-tabs-content id="size-vue">
+          <ContentRendererMarkdown class="text-xs" :value="MDSizeComponents" />
+        </km-tabs-content>
+      </km-tabs-group>
+      <h3 class="text-3xl font-semibold">Bordes</h3>
+      <km-tabs-group :tabs="borderTabsLists" id="border-buttons">
+        <km-tabs-content id="border-example">
+          <div class="flex flex-wrap gap-3">
+            <km-btn border="top" color="warning">border top</km-btn>
+            <km-btn border="left" color="danger">border left</km-btn>
+            <km-btn border="right" color="success">border right</km-btn>
+            <km-btn border="bottom" color="info">border bottom</km-btn>
+          </div>
+        </km-tabs-content>
+        <km-tabs-content id="border-vue">
+          <ContentRendererMarkdown
+            class="text-xs"
+            :value="MDBorderComponents"
+          />
+        </km-tabs-content>
+      </km-tabs-group>
     </div>
   </layouts-sections-components>
 </template>
 
-<script>
-export default {
-  name: "button",
-};
-</script>
+<script setup>
+const { data: MDComponents } = await useAsyncData("component-button", () =>
+  queryContent("/components/button/component").findOne(),
+);
+const { data: MDRoundedComponents } = await useAsyncData("rounded-button", () =>
+  queryContent("/components/button/rounded").findOne(),
+);
+const { data: MDFlatComponents } = await useAsyncData("flat-button", () =>
+  queryContent("/components/button/flat").findOne(),
+);
+const { data: MDOutlinedComponents } = await useAsyncData(
+  "outlined-button",
+  () => queryContent("/components/button/outlined").findOne(),
+);
+const { data: MDSizeComponents } = await useAsyncData("size-button", () =>
+  queryContent("/components/button/size").findOne(),
+);
+const { data: MDBorderComponents } = await useAsyncData("border-button", () =>
+  queryContent("/components/button/border").findOne(),
+);
 
-<style lang="scss" scoped></style>
+const componentTabsLists = [
+  {
+    name: "components",
+    target: "components",
+  },
+];
+
+const flatTabsLists = [
+  {
+    name: "example",
+    target: "flat-example",
+  },
+  {
+    name: "vue",
+    target: "flat-vue",
+  },
+];
+const roundedTabsLists = [
+  {
+    name: "example",
+    target: "rounded-example",
+  },
+  {
+    name: "vue",
+    target: "rounded-vue",
+  },
+];
+const outlinedTabsLists = [
+  {
+    name: "example",
+    target: "outlined-example",
+  },
+  {
+    name: "vue",
+    target: "outlined-vue",
+  },
+];
+const sizeTabsLists = [
+  {
+    name: "example",
+    target: "size-example",
+  },
+  {
+    name: "vue",
+    target: "size-vue",
+  },
+];
+const borderTabsLists = [
+  {
+    name: "example",
+    target: "border-example",
+  },
+  {
+    name: "vue",
+    target: "border-vue",
+  },
+];
+
+onMounted(() => {
+  stateMachineStore().initTabs();
+});
+</script>
--- pages/components/modal.vue
+++ pages/components/modal.vue
@@ -1,7 +1,7 @@
 <template>
   <layouts-sections-components>
     <div class="flex flex-col gap-4">
-      <h2 class="text-center text-6xl font-bold">Modales</h2>
+      <h2 class="text-center text-6xl font-bold">Modals</h2>
       <km-tabs-group :tabs="tabsLists" id="modals">
         <km-tabs-content id="components">
           <ContentRendererMarkdown class="text-xs" :value="MDcomponents" />
@@ -10,6 +10,112 @@
 
       <h3 class="text-3xl font-semibold">Posicion</h3>
       <km-tabs-group :tabs="positionTabsList" id="modalsPosition">
+        <km-tabs-content id="example">
+          <div class="relative min-h-[350px] flex flex-wrap gap-3">
+            <km-btn type="button" rounded="full" @click="topLeft = true">
+              top left
+            </km-btn>
+            <km-modal
+              v-model="topLeft"
+              position="top-left"
+              title="modal 1"
+              subtitle=""
+            >
+              top left
+            </km-modal>
+            <km-btn type="button" rounded="full" @click="topRight = true">
+              top right
+            </km-btn>
+            <km-modal
+              v-model="topRight"
+              position="top-right"
+              title="modal 1"
+              subtitle=""
+            >
+              top right
+            </km-modal>
+            <km-btn type="button" rounded="full" @click="topCenter = true">
+              top center
+            </km-btn>
+            <km-modal
+              v-model="topCenter"
+              position="top-center"
+              title="modal 1"
+              subtitle=""
+            >
+              top center
+            </km-modal>
+            <km-btn type="button" rounded="full" @click="bottomRight = true">
+              bottom right
+            </km-btn>
+            <km-modal
+              v-model="bottomRight"
+              position="bottom-right"
+              title="modal 1"
+              subtitle=""
+            >
+              bottom right
+            </km-modal>
+            <km-btn type="button" rounded="full" @click="bottomLeft = true">
+              bottom left
+            </km-btn>
+            <km-modal
+              v-model="bottomLeft"
+              position="bottom-left"
+              title="modal bottom left"
+              subtitle=""
+            >
+              bottom left
+            </km-modal>
+            <km-btn type="button" rounded="full" @click="bottomCenter = true">
+              bottom center
+            </km-btn>
+            <km-modal
+              v-model="bottomCenter"
+              position="bottom-center"
+              title="modal bottom center"
+              subtitle=""
+            >
+              bottom center
+            </km-modal>
+            <km-btn type="button" rounded="full" @click="centerRight = true">
+              center right
+            </km-btn>
+            <km-modal
+              v-model="centerRight"
+              position="center-right"
+              title="modal center right"
+              subtitle=""
+            >
+              center right
+            </km-modal>
+            <km-btn type="button" rounded="full" @click="centerLeft = true">
+              center left
+            </km-btn>
+            <km-modal
+              v-model="centerLeft"
+              position="center-left"
+              title="modal center left"
+              subtitle=""
+            >
+              center left
+            </km-modal>
+            <km-btn
+              type="button"
+              rounded="full"
+              @click="positionDefault = true"
+            >
+              position default
+            </km-btn>
+            <km-modal
+              v-model="positionDefault"
+              title="modal center center"
+              subtitle=""
+            >
+              center center
+            </km-modal>
+          </div>
+        </km-tabs-content>
         <km-tabs-content id="vue">
           <ContentRendererMarkdown
             class="text-xs"
@@ -18,169 +124,78 @@
         </km-tabs-content>
       </km-tabs-group>
 
-      <div class="flex flex-wrap gap-3">
-        <km-btn type="button" rounded="full" @click="topLeft = true">
-          top left
-        </km-btn>
-        <km-modal
-          v-model="topLeft"
-          position="top-left"
-          title="modal 1"
-          subtitle=""
-        >
-          top left
-        </km-modal>
-        <km-btn type="button" rounded="full" @click="topRight = true">
-          top right
-        </km-btn>
-        <km-modal
-          v-model="topRight"
-          position="top-right"
-          title="modal 1"
-          subtitle=""
-        >
-          top right
-        </km-modal>
-        <km-btn type="button" rounded="full" @click="topCenter = true">
-          top center
-        </km-btn>
-        <km-modal
-          v-model="topCenter"
-          position="top-center"
-          title="modal 1"
-          subtitle=""
-        >
-          top center
-        </km-modal>
-        <km-btn type="button" rounded="full" @click="bottomRight = true">
-          bottom right
-        </km-btn>
-        <km-modal
-          v-model="bottomRight"
-          position="bottom-right"
-          title="modal 1"
-          subtitle=""
-        >
-          bottom right
-        </km-modal>
-        <km-btn type="button" rounded="full" @click="bottomLeft = true">
-          bottom left
-        </km-btn>
-        <km-modal
-          v-model="bottomLeft"
-          position="bottom-left"
-          title="modal bottom left"
-          subtitle=""
-        >
-          bottom left
-        </km-modal>
-        <km-btn type="button" rounded="full" @click="bottomCenter = true">
-          bottom center
-        </km-btn>
-        <km-modal
-          v-model="bottomCenter"
-          position="bottom-center"
-          title="modal bottom center"
-          subtitle=""
-        >
-          bottom center
-        </km-modal>
-        <km-btn type="button" rounded="full" @click="centerRight = true">
-          center right
-        </km-btn>
-        <km-modal
-          v-model="centerRight"
-          position="center-right"
-          title="modal center right"
-          subtitle=""
-        >
-          center right
-        </km-modal>
-        <km-btn type="button" rounded="full" @click="centerLeft = true">
-          center left
-        </km-btn>
-        <km-modal
-          v-model="centerLeft"
-          position="center-left"
-          title="modal center left"
-          subtitle=""
-        >
-          center left
-        </km-modal>
-        <km-btn type="button" rounded="full" @click="positionDefault = true">
-          position default
-        </km-btn>
-        <km-modal
-          v-model="positionDefault"
-          title="modal center center"
-          subtitle=""
-        >
-          center center
-        </km-modal>
-      </div>
-      <h3 class="text-3xl font-semibold">tamaño</h3>
-      <div class="flex flex-wrap gap-3">
-        <km-tabs-group :tabs="sizeTabsList" id="size-modals">
-          <km-tabs-content id="size-vue">
-            <ContentRendererMarkdown
-              class="text-xs"
-              :value="MDSizeComponents"
-            />
-          </km-tabs-content>
-        </km-tabs-group>
+      <div class="flex flex-wrap gap-3"></div>
+      <h3 class="text-3xl font-semibold">Sizes</h3>
+      <km-tabs-group :tabs="sizeTabsList" id="size-modals">
+        <km-tabs-content id="size-example">
+          <div class="relative min-h-[350px] flex flex-wrap gap-3">
+            <km-btn type="button" rounded="full" @click="extraSmall = true">
+              extra small
+            </km-btn>
+            <km-modal
+              v-model="extraSmall"
+              title="modal extra small"
+              size="x-small"
+              subtitle=""
+            >
+              extra small
+            </km-modal>
+            <km-btn type="button" rounded="full" @click="small = true">
+              small
+            </km-btn>
+            <km-modal
+              v-model="small"
+              title="modal small"
+              size="small"
+              subtitle=""
+            >
+              small
+            </km-modal>
+            <km-btn type="button" rounded="full" @click="large = true">
+              large
+            </km-btn>
+            <km-modal
+              v-model="large"
+              title="modal large"
+              size="large"
+              subtitle=""
+            >
+              large
+            </km-modal>
+            <km-btn type="button" rounded="full" @click="extraLarge = true">
+              extra lager
+            </km-btn>
+            <km-modal
+              v-model="extraLarge"
+              title="modal extra large"
+              size="x-large"
+              subtitle=""
+            >
+              extra large
+            </km-modal>
+          </div>
+        </km-tabs-content>
+        <km-tabs-content id="size-vue">
+          <ContentRendererMarkdown class="text-xs" :value="MDSizeComponents" />
+        </km-tabs-content>
+      </km-tabs-group>
+      <h3 class="text-3xl font-semibold">Flat</h3>
+      <km-tabs-group :tabs="flatTabsList" id="flat-modals">
+        <km-tabs-content id="flat-example">
+          <div class="relative min-h-[350px] flex flex-wrap gap-3">
+            <km-btn type="button" rounded="full" @click="flat = true">
+              flat
+            </km-btn>
+            <km-modal v-model="flat" title="modal flat" flat subtitle="">
+              flat
+            </km-modal>
+          </div>
+        </km-tabs-content>
 
-        <km-btn type="button" rounded="full" @click="extraSmall = true">
-          extra small
-        </km-btn>
-        <km-modal
-          v-model="extraSmall"
-          title="modal extra small"
-          size="x-small"
-          subtitle=""
-        >
-          extra small
-        </km-modal>
-        <km-btn type="button" rounded="full" @click="small = true">
-          small
-        </km-btn>
-        <km-modal v-model="small" title="modal small" size="small" subtitle="">
-          small
-        </km-modal>
-        <km-btn type="button" rounded="full" @click="large = true">
-          large
-        </km-btn>
-        <km-modal v-model="large" title="modal large" size="large" subtitle="">
-          large
-        </km-modal>
-        <km-btn type="button" rounded="full" @click="extraLarge = true">
-          extra lager
-        </km-btn>
-        <km-modal
-          v-model="extraLarge"
-          title="modal extra large"
-          size="x-large"
-          subtitle=""
-        >
-          extra large
-        </km-modal>
-      </div>
-      <h3 class="text-3xl font-semibold">flat</h3>
-      <div class="flex flex-wrap gap-3">
-        <km-tabs-group :tabs="flatTabsList" id="size-modals">
-          <km-tabs-content id="flat-vue">
-            <ContentRendererMarkdown
-              class="text-xs"
-              :value="MDFlatComponents"
-            />
-          </km-tabs-content>
-        </km-tabs-group>
-        <km-btn type="button" rounded="full" @click="flat = true">
-          flat
-        </km-btn>
-        <km-modal v-model="flat" title="modal flat" flat subtitle="">
-          flat
-        </km-modal>
-      </div>
+        <km-tabs-content id="flat-vue">
+          <ContentRendererMarkdown class="text-xs" :value="MDFlatComponents" />
+        </km-tabs-content>
+      </km-tabs-group>
     </div>
   </layouts-sections-components>
 </template>
@@ -219,17 +234,29 @@ const idModal = "modal-1";
 
 const positionTabsList = [
   {
+    name: "example",
+    target: "example",
+  },
+  {
     name: "vue",
     target: "vue",
   },
 ];
 const flatTabsList = [
   {
+    name: "example",
+    target: "flat-example",
+  },
+  {
     name: "vue",
     target: "flat-vue",
   },
 ];
 const sizeTabsList = [
+  {
+    name: "example",
+    target: "size-example",
+  },
   {
     name: "vue",
     target: "size-vue",
