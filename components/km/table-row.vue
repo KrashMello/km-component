750cc451bea8b77a8170e9abf1df3302f27e1ee4
krashmello <joelenrikesoteldo@gmail.com>
2024-05-05 18:43:31 -0400

    feat: table design and usability

--- components/km/table-row.vue
+++ components/km/table-row.vue
@@ -1,6 +1,9 @@
 <template>
-  <tr class="bg-red-300 border-b dark:bg-gray-800 dark:border-gray-700">
-    <slot />
+  <tr :class="`${colors[color]}  hover:opacity-70  odd:opacity-85`">
+    <slot v-if="!!$slots.default" />
+    <td v-else v-for="(item, i) in items" :key="i" class="px-6 py-4">
+      {{ item }}
+    </td>
   </tr>
 </template>
 
@@ -9,7 +12,31 @@ export default {
   setup() {
     return {};
   },
+  data() {
+    return {
+      color: "",
+      colors: {
+        red: "bg-red-600/70 text-white dark:bg-red-300/70 dark:text-slate-600",
+        yellow:
+          "bg-yellow-600/70 text-white dark:bg-yellow-300/70 dark:text-slate-600",
+        cyan: "bg-cyan-600/70 text-white dark:bg-cyan-300/70 dark:text-slate-600",
+        blue: "bg-blue-600/70 text-white dark:bg-blue-300/70 dark:text-slate-600",
+        gray: "bg-gray-600/70 text-white dark:bg-gray-300/70 dark:text-slate-600",
+        slate:
+          "bg-slate-600/70 text-white dark:bg-slate-300/70 dark:text-slate-600",
+        purple:
+          "bg-purple-600/70 text-white dark:bg-purple-300/70 dark:text-slate-600",
+      },
+    };
+  },
+  props: {
+    items: {
+      type: Object,
+      required: true,
+    },
+  },
+  mounted() {
+    this.color = this.$parent.$props.color;
+  },
 };
 </script>
-
-<style lang="scss" scoped></style>
--- components/km/table.vue
+++ components/km/table.vue
@@ -1,15 +1,22 @@
 <template>
   <div
-    :class="`relative overflow-x-auto ${
-      shadow ? `${shadowColors[color]} shadow-lg` : ''
-    }`"
+    :class="`relative overflow-x-auto ${shadow ? 'drop-shadow-md' : ''} ${rounded ? 'rounded-lg' : ''}`"
   >
     <table
-      class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
+      :class="`w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 `"
     >
       <thead :class="`text-xs uppercase ${colors[color]}`">
         <tr>
-          <slot name="header" />
+          <slot v-if="!!$slots.header" name="header" />
+          <th
+            v-else
+            v-for="(item, i) in header"
+            :key="i"
+            scope="col"
+            class="px-6 py-3"
+          >
+            {{ item }}
+          </th>
         </tr>
       </thead>
       <tbody>
@@ -25,41 +32,39 @@ type colorType = "red" | "yellow" | "cyan" | "blue" | "gray" | "purple";
 export default {
   setup(props) {
     const colors = {
-      red: "",
-      yellow: "",
-      cyan: "",
-      blue: `${
-        props.dark ? "bg-blue-800 text-white" : "bg-blue-300 text-slate-600"
-      }`,
-      gray: "bg-gray-600 text-white",
-      purple: "",
-    };
-    const shadowColors = {
-      red: "",
-      yellow: "",
-      cyan: "",
-      blue: `${props.dark ? "shadow-blue-800" : "shadow-blue-300"}`,
-      gray: "shadow-gray-600",
-      purple: "",
+      red: "bg-red-600 text-white dark:bg-red-300 dark:text-slate-600",
+      yellow: "bg-yellow-600 text-white dark:bg-yellow-300 dark:text-slate-600",
+      cyan: "bg-cyan-600 text-white dark:bg-cyan-300 dark:text-slate-600",
+      blue: "bg-blue-600 text-white dark:bg-blue-300 dark:text-slate-600",
+      gray: "bg-gray-600 text-white dark:bg-gray-300 dark:text-slate-600",
+      slate: "bg-slate-600 text-white dark:bg-slate-300 dark:text-slate-600",
+      purple: "bg-purple-600 text-white dark:bg-purple-300 dark:text-slate-600",
     };
 
     return {
-      shadowColors,
       colors,
     };
   },
   props: {
     color: {
       type: String as PropType<colorType>,
-      default: "gray",
+      default: "slate",
     },
     dark: {
+      type: Boolean,
+      default: false,
+    },
+    rounded: {
       type: Boolean,
       default: false,
     },
     shadow: {
       type: Boolean,
       default: false,
+    },
+    header: {
+      type: Array,
+      Required: true,
     },
   },
 };
