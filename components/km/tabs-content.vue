70445b14d352fb269cf88e2b2906ec9ce5da5f82
krashmello <joelenrikesoteldo@gmail.com>
2024-05-05 18:41:20 -0400

    feat: tabs content dark color

--- components/km/tabs-content.vue
+++ components/km/tabs-content.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="hidden max-h-96 overflow-x-auto opacity-0 transition-opacity ease-in-out transform duration-300 p-4 rounded-lg bg-slate-600"
+    class="hidden max-h-96 overflow-x-auto opacity-0 transition-opacity ease-in-out transform duration-300 p-4 rounded-lg bg-slate-400 dark:bg-slate-600"
     :id="props.id"
   >
     <slot />
