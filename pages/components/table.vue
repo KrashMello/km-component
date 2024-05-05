ce5425409bf363dfbaef04e0106adec68038348d
krashmello <joelenrikesoteldo@gmail.com>
2024-05-05 18:42:32 -0400

    feat: adapter new layout and new components design

--- pages/components/table.vue
+++ pages/components/table.vue
@@ -1,44 +1,46 @@
 <template>
-  <layouts-sections-components>
-    <div class="flex flex-col gap-4">
-      <h2 class="text-center text-6xl font-bold">Tablas</h2>
-      <km-table>
-        <template #header>
-          <th scope="col" class="px-6 py-3">Product name</th>
-          <th scope="col" class="px-6 py-3">Color</th>
-          <th scope="col" class="px-6 py-3">Category</th>
-          <th scope="col" class="px-6 py-3">Price</th>
-        </template>
-        <km-table-row>
-          <th
-            scope="row"
-            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-          >
-            Apple MacBook Pro 17"
-          </th>
-          <td class="px-6 py-4">Silver</td>
-          <td class="px-6 py-4">Laptop</td>
-          <td class="px-6 py-4">$2999</td>
-        </km-table-row>
-      </km-table>
-      <km-table color="blue" shadow>
-        <template #header>
-          <th scope="col" class="px-6 py-3">codigo</th>
-          <th scope="col" class="px-6 py-3">Color</th>
-          <th scope="col" class="px-6 py-3">Category</th>
-          <th scope="col" class="px-6 py-3">Price</th>
-        </template>
-      </km-table>
-    </div>
-  </layouts-sections-components>
+  <div class="flex flex-col gap-4">
+    <h2 class="text-center text-6xl font-bold">Tablas</h2>
+    <km-table shadow rounded :header="datahead">
+      <km-table-row
+        v-for="(product, i) in products"
+        :key="i"
+        :items="product"
+      />
+    </km-table>
+    <km-table color="purple" shadow>
+      <template #header>
+        <th scope="col" class="px-6 py-3">codigo</th>
+        <th scope="col" class="px-6 py-3">Color</th>
+        <th scope="col" class="px-6 py-3">Category</th>
+        <th scope="col" class="px-6 py-3">Price</th>
+      </template>
+      <km-table-row
+        v-for="(product, i) in products"
+        :key="i"
+        :items="product"
+      />
+    </km-table>
+  </div>
 </template>
 
-<script>
-export default {
-  setup() {
-    return {};
+<script setup>
+definePageMeta({
+  layout: "docs",
+});
+const datahead = ["product name", "color", "category", "price"];
+const products = [
+  {
+    product: "Apple MacBook Pro 17",
+    color: "Silver",
+    category: "Laptop",
+    price: "$2999",
   },
-};
+  {
+    product: "Asus Zenbook",
+    color: "Silver",
+    category: "Laptop",
+    price: "$2999",
+  },
+];
 </script>
-
-<style lang="scss" scoped></style>
